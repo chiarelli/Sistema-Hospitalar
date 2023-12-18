c8e6c4fb5145f23163e12d66a6a00b50ff79bc1f
Raphael Mathias <oi.sucesso@gmail.com>
2023-12-18 17:01:15 -0300

    Postgresql driver

---
+++ hospital-system/src/main/java/com/chiarelli/hospitalsystem/identityAndAccess/config/PostgresDataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.chiarelli.hospitalsystem.identityAndAccess.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class PostgresDataSourceConfig {
+
+    @Bean
+    public DataSource postgresDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/test");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("123");
+
+        return dataSource;
+    }
+
+}
